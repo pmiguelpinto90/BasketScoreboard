f148959cb5a3e9630ccb0a3a812f6be0e72bf7d9
MiguelPynto <miguelpynto@outlook.com>
2017-09-16 01:15:24 +0100

    Updated to ShiftDisplay 3.6.0 API, more work on counter logic.

--- scoreboard.ino
+++ scoreboard.ino
@@ -1,72 +1,75 @@
 #include <ShiftDisplay.h>
 #include "commands.h"
 
+const int DISPLAY_TYPE = INDIVIDUAL_ANODE; // TODO in lib
 const int COUNTER_DISPLAY_SIZE = 4;
-const int SCORE_DISPLAY_QUANTITY = 5; // TODO 
-const int SCORE_DISPLAY_SIZES[] = {2, 1, 2, 1, 1}; // TODO 
-enum ScoreDisplayId { // TODO 
-	POINTS_HOME,
-	PERIOD,
-	POINTS_VISIT,
-	FOULS_HOME,
-	FOULS_VISIT,
+const int SCORE_SECTION_COUNT = 5;
+const int SCORE_SECTION_SIZES[] = {2, 1, 2, 1, 1};
+enum sections { // TODO
+	SECTION_POINTS_HOME,
+	SECTION_PERIOD,
+	SECTION_POINTS_VISIT,
+	SECTION_FOULS_HOME,
+	SECTION_FOULS_VISIT,
 };
-const int TEN_HOME_INDEX = 5; // digit index where the one from home fouls is connected
-const int TEN_VISIT_INDEX = 6; // digit index where the one from visit fouls is connected
-const int POSSESSION_HOME_INDEX = 0; // digit index where home possession symbol is connected
-const int POSSESSION_VISIT_INDEX = 1; // digit index where visit possession symbol is connected
 
-const int COUNTERS[] = {5, 8}; // in minutes TODO 
+const int COUNTERS_COUNT = 2;
+const int COUNTERS[] = {5, 8}; // in minutes
+const int DEFAULT_COUNTER_ID = 0;
 
 volatile bool counterOn;
-volatile bool counterReset;
-
-const int DISPLAY_TYPE = 0; // TODO in lib shiftdisplay SINGULAR_CATHODE UNIQUE_CATHODE
+volatile bool buttonCounterReset;
+volatile bool possessionHome;
+volatile bool possessionVisit;
 
 ShiftDisplay counterDisplay(DISPLAY_TYPE, COUNTER_DISPLAY_SIZE);
-ShiftDisplay scoreDisplay(DISPLAY_TYPE, SCORE_DISPLAY_QUANTITY, SCORE_DISPLAY_SIZES);
+ShiftDisplay scoreDisplay(DISPLAY_TYPE, SCORE_SECTION_COUNT, SCORE_SECTION_SIZES);
 
 void setPointsHome(int points) {
-	scoreDisplay.setAt(POINTS_HOME, points);
+	scoreDisplay.setAt(SECTION_POINTS_HOME, points);
+	if (possessionHome)
+		scoreDisplay.setDotAt(SECTION_POINTS_HOME, 0, true); // set home possession symbol
 }
 
 void setPointsVisit(int points) {
-	scoreDisplay.setAt(POINTS_VISIT, points);
+	scoreDisplay.setAt(SECTION_POINTS_VISIT, points);
+	if (possessionVisit)
+		scoreDisplay.setDotAt(SECTION_POINTS_VISIT, 0, true); // set visit possession symbol
 }
 
 void setFoulsHome(int fouls) {
-	scoreDisplay.setAt(FOULS_HOME, fouls);
-	if (fouls > 9)
-		scoreDisplay.insertDot(TEN_HOME_INDEX);
-	else
-		scoreDisplay.removeDot(TEN_HOME_INDEX);
+	scoreDisplay.setAt(SECTION_FOULS_HOME, fouls); // only the least significant digit will be set
+	scoreDisplay.setDotAt(SECTION_FOULS_HOME, 0, fouls > 9); // character '1' is connected as being the dot in home fouls
 }
 
 void setFoulsVisit(int fouls) {
-	scoreDisplay.setAt(FOULS_VISIT, fouls);
-	if (fouls > 9)
-		scoreDisplay.insertDot(TEN_VISIT_INDEX); // TODO func setDot at lib shiftdisplay
-	else
-		scoreDisplay.removeDot(TEN_VISIT_INDEX);
+	scoreDisplay.setAt(SECTION_FOULS_VISIT, fouls); // only the least significant digit will be set
+	scoreDisplay.setDotAt(SECTION_FOULS_VISIT, 0, fouls > 9); // character '1' is connected as being the dot in visit fouls
 }
 
 void setPeriod(int period) {
-	scoreDisplay.setAt(PERIOD, period);
+	scoreDisplay.setAt(SECTION_PERIOD, period);
 }
 
 void setPossessionHome() {
-	scoreDisplay.removeDot(POSSESSION_VISIT_INDEX);
-	scoreDisplay.insertDot(POSSESSION_HOME_INDEX);
+	scoreDisplay.setDotAt(SECTION_POINTS_VISIT, 0, false);
+	scoreDisplay.setDotAt(SECTION_POINTS_HOME, 0, true);
+	possessionVisit = false;
+	possessionHome = true;
 }
 
 void setPossessionVisit() {
-	scoreDisplay.removeDot(POSSESSION_HOME_INDEX);
-	scoreDisplay.insertDot(POSSESSION_VISIT_INDEX);
+	scoreDisplay.setDotAt(SECTION_POINTS_HOME, 0, false);
+	scoreDisplay.setDotAt(SECTION_POINTS_VISIT, 0, true);
+	possessionHome = false;
+	possessionVisit = true;
 }
 
 void clearPossession() {
-	scoreDisplay.removeDot(POSSESSION_HOME_INDEX);
-	scoreDisplay.removeDot(POSSESSION_VISIT_INDEX);
+	scoreDisplay.setDotAt(SECTION_POINTS_HOME, 0, false);
+	scoreDisplay.setDotAt(SECTION_POINTS_VISIT, 0, false);
+	possessionHome = false;
+	possessionVisit = false;
 }
 
 void receiveEvent(int size) {
@@ -77,7 +80,7 @@ void receiveEvent(int size) {
 			counterOn = !counterOn;
 			break;
 		case RESET_COUNTER:
-			counterReset = true;
+			buttonCounterReset = true;
 			break;
 		case SET_POINTS_HOME:
 			setPointsHome(arg);
@@ -107,36 +110,61 @@ void receiveEvent(int size) {
 }
 
 void setup() {
-	counterOn, counterReset = false;
+	counterOn, buttonCounterReset = false;
+	possessionHome, possessionVisit = false;
 	counterDisplay.set("0000"); // TODO
 	//onReceive(receiveEvent); // TODO
 }
 
 void loop() {
-	static unsigned long lastTime = 0;
+	static int counterId = DEFAULT_COUNTER_ID;
+	static long counter = COUNTERS[counterId] * 60 * 100; 
 	static bool counterSet = true;
+	static unsigned long lastTime = 0;
 
 	unsigned long currTime = millis() / 10;
 
 	if (counterOn && currTime != lastTime) {
-		lastTime = currTime;
+		counter--;
+		if (counter == 0) {
+			counterDisplay.set(" 000");
+			counterOn = false;
+		} else if (counter < 10) {
+			//TODO
+			counterDisplay.set();
+		} else if (counter < 100) {
+			//TODO
+			counterDisplay.set();
+		} else if (counter < 6000) {
+			//TODO
+			counterDisplay.set();
+		} else {
+			int seconds = counter / 100;
+			int min = seconds / 60;
+			int sec = seconds % 60;
+			//TODO
+			counterDisplay.set();
+		}
+
 		counterSet = false;
-		// TODO update counterDisplay
+		lastTime = currTime;
 	}
 
-	if (counterReset) { // button pressed
+	if (buttonCounterReset) { // button pressed
 		if (counterSet) {
 			// iterate counter time
-			/*counter = (counter + 1) % sizeof(COUNTERS); //TODO sizeof
-			int minutes = COUNTERS[counter];
-			counterDisplay.set();*/
+			counterId = (counterId + 1) % COUNTERS_COUNT;
+			int minutes = COUNTERS[counterId];
+			//TODO
+			counterDisplay.set();
 		} else {
 			// stop and reset counter
 			counterOn = false;
 			counterSet = true;
-			/*int minutes = COUNTERS[counter];
-			counterDisplay.set();*/
+			int minutes = COUNTERS[counterId];
+			//TODO
+			counterDisplay.set();
 		}
-		counterReset = false;
+		buttonCounterReset = false;
 	}
 }
